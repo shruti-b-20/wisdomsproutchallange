338c45fc2d2a385f4b182ecbac1f97556a0b8286
shruti-b-20 <163502015+shruti-b-20@users.noreply.github.com>
2025-08-26 23:43:06 +0530

    Add files via upload

---
+++ day12.py
@@ -0,0 +1,25 @@
+def isValid(s: str) -> bool:
+    mapping = {')': '(', '}': '{', ']': '['}
+    stack = []
+
+    for char in s:
+        if char in mapping:
+            top_element = stack.pop() if stack else '#'
+            if mapping[char] != top_element:
+                return False
+        else:
+            stack.append(char)
+
+    return not stack
+
+#TESTCASES
+
+print(isValid("()"))       
+print(isValid("([)]"))     
+print(isValid("[{()}]"))   
+print(isValid(""))         
+print(isValid("{[}"))      
+print(isValid("]"))        
+print(isValid("((("))  
+    
+
